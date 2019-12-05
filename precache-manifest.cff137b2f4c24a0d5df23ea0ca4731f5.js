2cecd2f81afe37de06717457ccb01b8f2540a5de
Sergio Dias <sergio_diasfilho@hotmail.com>
2019-12-05 18:37:45 -0300

    Updates

---
+++ precache-manifest.cff137b2f4c24a0d5df23ea0ca4731f5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01f8273f60c14ba8cf07087be534088",
+    "url": "/timesAvenida/index.html"
+  },
+  {
+    "revision": "48009ddf65395fd729b6",
+    "url": "/timesAvenida/static/js/2.ff4c2bb7.chunk.js"
+  },
+  {
+    "revision": "8d39acf757166da5bbc3",
+    "url": "/timesAvenida/static/js/main.2ab5ff71.chunk.js"
+  },
+  {
+    "revision": "2e1cce58add3a4481ed4",
+    "url": "/timesAvenida/static/js/runtime-main.ff4011c5.js"
+  }
+]);
